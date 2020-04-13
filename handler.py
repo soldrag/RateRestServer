b40d6f6a8e5123994f8236c6a309143bf0dbec90
soldrag <soldrag@gmail.com>
2020-04-13 13:43:49 +0300

    initial commit

--- handler.py
+++ handler.py
@@ -1,4 +1,14 @@
+from urllib import request
+import json
 
 
-if __name__ == "__main__"
-    pass
+get_url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+
+
+def get_rate(url: str) -> float:
+    resp = dict(json.loads(request.urlopen(url).read()))
+    return float(resp['Valute']['USD']['Value'])
+
+
+if __name__ == "__main__":
+    print(get_rate(get_url))
